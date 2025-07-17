a1076288f152e4d417ce20191ee78185b60f0139
niamorg <romaing.bis@gmail.com>
2025-07-17 15:40:26 +0200

    rename my envs

--- gym_lowcostrobot/envs/__init__.py
+++ gym_lowcostrobot/envs/__init__.py
@@ -1,10 +1,10 @@
 from .lift_cube_env import LiftCubeEnv
 from .pick_place_cube_env import PickPlaceCubeEnv
-from .push_cube_env import PushCubeEnv
-from .reach_cube_env import ReachCubeEnv
+from .old_push_cube_env import PushCubeEnv
+from .old_reach_cube_env import ReachCubeEnv
 from .stack_two_cubes_env import StackTwoCubesEnv
 from .push_cube_loop_env import PushCubeLoopEnv
-from .my_push_cube_env import MyPushCubeEnv
-from .my_reach_cube_env import MyReachCubeEnv
+from .push_cube_env import MyPushCubeEnv
+from .reach_cube_env import MyReachCubeEnv
 
 __all__ = ["LiftCubeEnv", "PickPlaceCubeEnv", "PushCubeEnv", "ReachCubeEnv", "StackTwoCubesEnv", "PushCubeLoopEnv", "MyPushCubeEnv"]
